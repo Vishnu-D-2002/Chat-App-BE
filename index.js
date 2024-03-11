628d9ebf648050fc99864662add2259ca4f2e27c
Vishnu-D-2002 <vishnulosangels@gmail.com>
2024-03-11 18:23:51 +0530

    origin added

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ mongoose.connect(MongoDB_URL).then(() => {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://chat-in-app.netlify.app/"],
     methods: ["GET", "POST"],
   },
 });
