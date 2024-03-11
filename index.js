9567ed84fab34e94577b5e61ae87e4126e8d28fe
Vishnu-D-2002 <vishnulosangels@gmail.com>
2024-03-11 18:15:47 +0530

    socket

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const { MongoDB_URL, PORT } = require('./utils');
-const userRouter = require('./Routes/userRoute');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { MongoDB_URL, PORT } = require("./utils");
+const userRouter = require("./Routes/userRoute");
+const { Server } = require("socket.io");
+const http = require("http");
 
 const app = express();
+const server = http.createServer(app);
 
-mongoose.set('strictQuery', false);
-
-console.log('Connecting to MongoDB...');
-
-mongoose.connect(MongoDB_URL)
-    .then(() => {
-        console.log('Connected to MongoDB');
-        app.listen(PORT, () => {
-            console.log(`Server is running in http://localhost:${PORT}`);
-        })
-    });
+mongoose.set("strictQuery", false);
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/', userRouter);
+console.log("Connecting to MongoDB...");
+
+mongoose.connect(MongoDB_URL).then(() => {
+  console.log("Connected to MongoDB");
+  server.listen(PORT, () => {
+    console.log(`Server is running in http://localhost:${PORT}`);
+  });
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("join_room", (data) => {
+      socket.join(data);
+      console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
+
+    socket.on("send_message", (data) => {
+      socket.to(data.room).emit("receive_message", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User Disconnected", socket.id);
+    });
+});
+
+app.use("/", userRouter);
