8bd4459ded9396d4f40a105461a3a2d8c27c6a3b
Vishnu-D-2002 <vishnulosangels@gmail.com>
2024-03-14 08:27:18 +0530

    chated users fetched

--- index.js
+++ index.js
@@ -62,24 +62,30 @@ io.on("connection", (socket) => {
     console.log("Client disconnected");
   });
 });
+
 app.use("/", userRouter);
 app.use('/msg', messageRouter);
-app.get('/users/with-messages/:userId', async (req, res) => {
+
+app.get("/users/with-messages/:userId", async (req, res) => {
   try {
     const userId = req.params.userId;
 
-    // Find messages where the logged-in user is the receiver
-    const messages = await Message.find({ receiver: userId }).select('sender');
+    const messages = await Message.find({
+      $or: [{ sender: userId }, { receiver: userId }],
+    }).select("sender receiver");
 
-    // Extract sender IDs from the messages
     const senderIds = messages.map((message) => message.sender);
+    const receiverIds = messages.map((message) => message.receiver);
 
-    // Find users based on the sender IDs
-    const usersWithMessages = await User.find({ _id: { $in: senderIds } });
+    let allUserIds = [...new Set([...senderIds, ...receiverIds])];
+
+    const usersWithMessages = await User.find({
+      _id: { $in: allUserIds, $ne: userId },
+    });
 
     res.json({ usersWithMessages });
   } catch (error) {
-    console.error('Error fetching users with messages:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error fetching users with messages:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
